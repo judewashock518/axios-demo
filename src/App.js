43961f093e8dc6155fc84afce30a2fbcd6cc754d
JW <114599894+judewashock518@users.noreply.github.com>
2023-02-05 14:32:15 -0500

    greated 'async' function for axios get request and created 'get all songs' button

--- src/App.js
+++ src/App.js
@@ -2,18 +2,26 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 
-
-
 function App() {
 
+  const [songs, setSongs] = useState([]);
+
   useEffect(() => {
-    const response = await axios.get('');
+    getAllSongs();
+    console.log('Hello World');
   }, []);
+
+  async function getAllSongs(){
+    const response = await axios.get('http://127.0.0.1:8000/api/music/');
+    console.log(response.data);
+    setSongs(response.data)
+  }
 
 
   return (
     <div>
- 
+      
+      <button onClick={() => getAllSongs()}> Get All Songs </button>
     </div>
   );
 }
